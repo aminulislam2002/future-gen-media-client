05451a589bab65a11e1abb5e8d31830ad73193d4
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-09-29 02:33:20 -0700

    add top banner

--- src/Pages/Home/TopBanner/TopBanner.jsx
+++ src/Pages/Home/TopBanner/TopBanner.jsx
@@ -1,16 +1,14 @@
-import bannerImage from "../../../assets/images/Banner/html-css-collage-concept-with-person.jpg"
+import bannerImage from "../../../assets/images/Banner/html-css-collage-concept-with-person.jpg";
 
 const TopBanner = () => {
-    return (
-        <div>
-            <div>
-                <img src={bannerImage} alt="Banner Image" />
-            </div>
-            <div>
-                
-            </div>
-        </div>
-    );
+  return (
+    <div className="max-h-screen">
+      <div>
+        <img src={bannerImage} className="h-screen w-screen" alt="Banner Image" />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default TopBanner;
